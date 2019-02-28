6b7edbe05ad98b207a501286d050570f5c4c4010
ohue_ruth <ohue_ruth@live.fr>
2019-02-28 11:02:34 +0100

    Commit initial

---
+++ src/main/java/projet_collaboratif/ProjetCollaboratifApplication.java
@@ -0,0 +1,13 @@
+package projet_collaboratif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCollaboratifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCollaboratifApplication.class, args);
+	}
+
+}
