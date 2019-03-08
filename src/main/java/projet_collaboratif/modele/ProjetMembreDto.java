0ee4c409eaa4af60425d5730910901f717560e70
ohue-ruth <ohue_ruth@live.fr>
2019-03-08 15:13:52 +0100

    Modification du contructeur Ajout de champ projetDao et equipeDao utilisé dans le constructeur

--- src/main/java/projet_collaboratif/modele/ProjetMembreDto.java
+++ src/main/java/projet_collaboratif/modele/ProjetMembreDto.java
@@ -2,6 +2,12 @@ package projet_collaboratif.modele;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import projet_collaboratif.dao.EquipeDao;
+import projet_collaboratif.dao.PersonneDao;
+import projet_collaboratif.dao.ProjetDao;
+
 /**
  * Projet affecté à un membre (à une de ses sessions de formation). Si le membre
  * n'est dans aucune équipe, getEquipe() vaut null.
@@ -18,12 +24,28 @@ public class ProjetMembreDto {
 	/** Null si dans aucune equipe */
 	private Equipe equipeDuMembre;
 
-	public ProjetMembreDto(Projet projet, int nbEquipes, int idMembre,
-			Equipe equipeDuMembre) {
-		this.projet = projet;
+//	public ProjetMembreDto(Projet projet, int nbEquipes, int idMembre,
+//			Equipe equipeDuMembre) {
+//		this.projet = projet;
+//		this.nbEquipes = nbEquipes;
+//		this.idMembre = idMembre;
+//		this.equipeDuMembre = equipeDuMembre;
+//	}
+
+	@Autowired
+	ProjetDao projetDao;
+
+	@Autowired
+	EquipeDao equipeDao;
+
+	public ProjetMembreDto(int idProjet, int nbEquipes, int idMembre, Integer idEquipeDuMembre) {
+
+		this.projet = projetDao.findByIdProjet(idProjet);
 		this.nbEquipes = nbEquipes;
 		this.idMembre = idMembre;
-		this.equipeDuMembre = equipeDuMembre;
+		if (idEquipeDuMembre != null) {
+			this.equipeDuMembre = equipeDao.findByIdEquipe(idEquipeDuMembre);
+		}
 	}
 
 	public Projet getProjet() {
