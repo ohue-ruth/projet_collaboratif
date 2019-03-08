51dc661d205761561fc3a4d1b70e6cdd9a638039
ohue-ruth <ohue_ruth@live.fr>
2019-03-08 15:15:54 +0100

    Création de l'interface EquipeDao Ajout de la méthode findByIdEquipe

--- src/main/java/projet_collaboratif/dao/EquipeDao.java
+++ src/main/java/projet_collaboratif/dao/EquipeDao.java
@@ -3,7 +3,8 @@ package projet_collaboratif.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import projet_collaboratif.modele.Equipe;
+import projet_collaboratif.modele.Projet;
 
-public interface EquipeDao extends 
-	CrudRepository<Equipe, Integer> {
+public interface EquipeDao extends CrudRepository<Equipe, Integer> {
+	public Equipe findByIdEquipe(Integer IdEquipe);
 }
