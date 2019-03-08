657cc2420082df59f5fb486ce36eec4b77f2828e
ohue-ruth <ohue_ruth@live.fr>
2019-03-08 16:22:53 +0100

    Modifiacation en cours de le requête sur la méthode findByIdPersonneNative

--- src/main/java/projet_collaboratif/dao/ProjetDao.java
+++ src/main/java/projet_collaboratif/dao/ProjetDao.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import projet_collaboratif.modele.Personne;
 import projet_collaboratif.modele.Projet;
+import projet_collaboratif.modele.ProjetMembreDto;
 
 public interface ProjetDao extends CrudRepository<Projet, Integer>  {
 	//@Query("select * from projet where projet.id_promotion = :idPromotion and date_fin > CURRENT_TIMESTAMP")
@@ -15,4 +16,11 @@ public interface ProjetDao extends CrudRepository<Projet, Integer>  {
 	public ArrayList<Projet> findProjetsByIdPromotionNative(@Param("idPromotion") Integer idPromotion);
 	
 	public Projet findByIdProjet(Integer IdProjet);
+	
+	@Query(value = "SELECT new projet_collaboratif.modele.ProjetMembreDto(pr.id_projet, COUNT(DISTINCT toutes_equipes.id_equipe) nb_equipes, :idPersonne, me_etudiant.id_equipe) FROM projet pr LEFT JOIN membre_equipe toutes_equipes ON pr.id_projet = toutes_equipes.id_projet LEFT JOIN membre_equipe me_etudiant ON pr.id_projet = me_etudiant.id_projet WHERE pr.date_fin > NOW() AND pr.id_promotion IN ( SELECT id_promotion FROM membre_promotion WHERE id_personne = :idPersonne ) AND me_etudiant.id_personne = :idPersonne", nativeQuery = true)
+	public ArrayList<ProjetMembreDto> findByIdPersonneNative(@Param("idPersonne") Integer idPersonne);
+
+//	@Query(value = "SELECT mep FROM membre_promotion mep where mep.idPersonne = :idPersonne")
+//	public ArrayList<ProjetMembreDto> findByIdPersonne(@Param("idPersonne") Integer idPersonne);
+	
 }
