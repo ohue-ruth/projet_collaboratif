b77420b4d4589cdc4086e1cf420a602a5437a3e1
ohue-ruth <ohue_ruth@live.fr>
2019-03-08 16:24:16 +0100

    Ajout de la méthode findByIdPersonneNative

--- src/main/java/projet_collaboratif/service/ProjetService.java
+++ src/main/java/projet_collaboratif/service/ProjetService.java
@@ -7,7 +7,10 @@ import org.springframework.data.repository.query.Param;
 
 import projet_collaboratif.modele.Personne;
 import projet_collaboratif.modele.Projet;
+import projet_collaboratif.modele.ProjetMembreDto;
 
 public interface ProjetService {
 	public ArrayList<Projet> findProjetsByIdPromotionNative(Integer idPromotion);
+	
+	public ArrayList<ProjetMembreDto> findByIdPersonneNative(Integer idPersonne);
 }
